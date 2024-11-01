c91744da841f232161913549d72d9860996b96ee
vignesh-prakash <vignesh24p@gmail.com>
2024-11-01 21:31:33 +0530

    Update CheckTempeartureBreach.h

--- CheckTempeartureBreach.h
+++ CheckTempeartureBreach.h
@@ -24,3 +24,5 @@ typedef struct {
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit);
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
+
+void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
