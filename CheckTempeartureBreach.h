c9955dcaf1be3a77aca557937ef426c34265c96a
vignesh-prakash <vignesh24p@gmail.com>
2024-11-01 20:37:20 +0530

    Update and rename typewise-alert.h to CheckTempeartureBreach.h

---
+++ CheckTempeartureBreach.h
@@ -0,0 +1,26 @@
+#pragma once
+
+typedef enum {
+  PASSIVE_COOLING,
+  HI_ACTIVE_COOLING,
+  MED_ACTIVE_COOLING
+} CoolingType;
+
+typedef enum {
+  NORMAL,
+  TOO_LOW,
+  TOO_HIGH
+} BreachType;
+
+typedef enum {
+  TO_CONTROLLER,
+  TO_EMAIL
+} AlertTarget;
+
+typedef struct {
+  CoolingType coolingType;
+  char brand[48];
+} BatteryCharacter;
+
+BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
