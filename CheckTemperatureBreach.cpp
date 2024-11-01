2cff0e2a4189d653388e4a8ff9da43a9f659068b
vignesh-prakash <vignesh24p@gmail.com>
2024-11-01 23:55:44 +0530

    Update CheckTemperatureBreach.cpp

--- CheckTemperatureBreach.cpp
+++ CheckTemperatureBreach.cpp
@@ -31,3 +31,17 @@ BreachType classifyTemperatureBreach(
   }
   return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
+
+void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+
+  BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
+
+  switch(alertTarget) {
+    case TO_CONTROLLER:
+      sendToController(breachType);
+      break;
+    case TO_EMAIL:
+      sendToEmail(breachType);
+      break;
+  }
+}
