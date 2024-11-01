f7a8d6fd5aefd651f35ba1ec808bea6dc8b37102
vignesh-prakash <vignesh24p@gmail.com>
2024-11-01 20:05:38 +0530

    Create AlertTemperaturebreach.cpp

---
+++ AlertTemperaturebreach.cpp
@@ -0,0 +1,22 @@
+
+
+void sendToController(BreachType breachType) {
+  const unsigned short header = 0xfeed;
+  printf("%x : %x\n", header, breachType);
+}
+
+void sendToEmail(BreachType breachType) {
+  const char* recepient = "a.b@c.com";
+  switch(breachType) {
+    case TOO_LOW:
+      printf("To: %s\n", recepient);
+      printf("Hi, the temperature is too low\n");
+      break;
+    case TOO_HIGH:
+      printf("To: %s\n", recepient);
+      printf("Hi, the temperature is too high\n");
+      break;
+    case NORMAL:
+      break;
+  }
+}
