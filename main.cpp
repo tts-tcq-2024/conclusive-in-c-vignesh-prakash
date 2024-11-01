78aeeffcfbf6236c25c089dacb2517d25d5da9fe
vignesh-prakash <vignesh24p@gmail.com>
2024-11-01 23:45:53 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,9 @@
 #include "AlertTemparatureBreach.h"
 #include <stdio.h>
 
-void checkAndAlert(
-    AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
 
-  BreachType breachType = classifyTemperatureBreach(
-    batteryChar.coolingType, temperatureInC
-  );
+  BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
 
   switch(alertTarget) {
     case TO_CONTROLLER:
