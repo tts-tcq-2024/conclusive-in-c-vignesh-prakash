25b90090505c847c967ffbeb07093016f337bb4f
vignesh-prakash <vignesh24p@gmail.com>
2024-11-01 19:49:43 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "typewise-alert.h"
+#include <stdio.h>
+
+void checkAndAlert(
+    AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+
+  BreachType breachType = classifyTemperatureBreach(
+    batteryChar.coolingType, temperatureInC
+  );
+
+  switch(alertTarget) {
+    case TO_CONTROLLER:
+      sendToController(breachType);
+      break;
+    case TO_EMAIL:
+      sendToEmail(breachType);
+      break;
+  }
+}
