5f12f4395249abc570bc8c76f4f719df03c05a13
vignesh-prakash <vignesh24p@gmail.com>
2024-11-02 00:00:52 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@
 #include <stdio.h>
 
 void main() {
-  
+    AlertTarget alertTarget = TO_CONTROLLER;
+   
+    BatteryCharacter batteryChar;
+    batteryChar.coolingType = PASSIVE_COOLING;
+    strncpy(batteryChar.brand, "BOSCH", sizeof(batteryChar.brand) - 1);
+    batteryChar.brand[sizeof(batteryChar.brand) - 1] = '\0';
+
+    double temperatureInC = 40.0;
+    
+    checkAndAlert(alertTarget,batteryChar,temperatureInC);
 }
 
